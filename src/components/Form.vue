5883ef99fb0b9d0e255c902ac9b6a72d910bc2cc
SokolArr <alex970899@gmail.com>
2021-11-10 01:25:01 +0300

    Сделал основную часть задания

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -6,68 +6,159 @@
         <!--      ++++++++++++++++++++++++-->
         <div class="input-wrapper second-name">
           Фамилия
-          <input class="myInput second-name" type="text" placeholder="Иванов" v-model="secondName" />
+          <input
+            class="myInput second-name"
+            type="text"
+            placeholder="Иванов"
+            v-model="secondName"
+          />
         </div>
         <!--      ++++++++++++++++++++++++-->
         <div class="input-wrapper first-name">
           Имя
-          <input class="myInput first-name" type="text" placeholder="Иван" v-model="firstName" />
+          <input
+            class="myInput first-name"
+            type="text"
+            placeholder="Иван"
+            v-model="firstName"
+          />
         </div>
         <!--      ++++++++++++++++++++++++-->
         <div class="input-wrapper patronymic-name">
           Отчество
-          <input class="myInput patronymic-name" type="text" placeholder="Иванович" v-model="patronymicName" />
+          <input
+            class="myInput patronymic-name"
+            type="text"
+            placeholder="Иванович"
+            v-model="patronymicName"
+          />
         </div>
         <!--      ++++++++++++++++++++++++-->
       </div>
       <div class="string two">
-        <div class="birthday" style="display: flex; flex-direction: column;width: fit-content">
+        <div
+          class="birthday"
+          style="display: flex; flex-direction: column; width: fit-content"
+        >
           Дата рождения
-          <input class="myInput birthday" placeholder="дд.мм.гггг" v-model="birthdayText" />
+          <input
+            class="myInput birthday"
+            placeholder="дд.мм.гггг"
+            v-model="birthdayText"
+          />
         </div>
       </div>
       <div class="string three">
-        <div class="email" style="display: flex; flex-direction: column;width: fit-content">
+        <div
+          class="email"
+          style="display: flex; flex-direction: column; width: fit-content"
+        >
           E-mail
-          <input class="myInput email" placeholder="example@email.com" v-model="emailText" />
+          <input
+            class="myInput email"
+            placeholder="example@email.com"
+            v-model="emailText"
+          />
         </div>
       </div>
     </div>
 
-
-    <div class="sex" style="display: flex; flex-direction: column;margin-top: 10px">
+    <div
+      class="sex"
+      style="display: flex; flex-direction: column; margin-top: 10px"
+    >
       <h2>Пол</h2>
       <div class="male-female-wrapper">
-        <input type="radio" value="Мужской" id="radio-male" class="radio" v-model="sexText" />
+        <input
+          type="radio"
+          value="Мужской"
+          id="radio-male"
+          class="radio"
+          v-model="sexText"
+        />
         <label for="radio-male" class="label-radio">Мужской</label>
-        <input type="radio" value="Женский" id="radio-female" class="radio" v-model="sexText" />
+        <input
+          type="radio"
+          value="Женский"
+          id="radio-female"
+          class="radio"
+          v-model="sexText"
+        />
         <label for="radio-female" class="label-radio">Женский</label>
       </div>
     </div>
 
-
-    <div class="passport-data" style="display: flex; flex-direction: column;margin-top: 10px">
+    <div
+      class="passport-data"
+      style="display: flex; flex-direction: column; margin-top: 10px"
+    >
       <h2>Пасспортные данные</h2>
-      <div class="name citizenship" style="display: flex;flex-direction: column; width: 300px">
-        Гражданство
-        <input class="myInput" v-model="citizenshipText" />
+      Гражданство
+      <div
+        class="name citizenship"
+        style="display: flex; flex-direction: column; width: 300px"
+        v-click-outside="hideDropdownCitizenship"
+      >
+        <div>
+
+        </div>
+        <div
+          class="dropdown-wrapper"
+          @click="isDropdownCitizenshipOpen=true"
+        >
+          <input
+            class="myInput dropdown citizenship"
+            v-model="citizenshipText"
+            @focus="isDropdownCitizenshipOpen=true"
+            ref="citizenship"
+            style="border: 0px; margin: 0"
+          />
+          <div class="triangle"></div>
+        </div>
+        <div
+          class="dropdown"
+          v-if="isDropdownCitizenshipOpen"
+        >
+          <ul class="dropdown-content">
+            <li
+              v-for="citizenship in citizenshipsData"
+              :key="citizenship.id"
+              class="li-in-content"
+              @click="clickHandlerOnCitizenship(citizenship)"
+            >
+              {{citizenship.nationality}}
+            </li>
+          </ul>
+        </div>
+
       </div>
 
-      <div
-        class="string four"
-        v-if="citizenshipText==='Россия'"
-      >
+
+
+      <div class="string four" v-if="citizenshipText === 'Россия' || citizenshipText === 'Russia'">
         <div class="passport serial">
           Серия пасспорта
-          <input class="myInput passSerial" placeholder="1234" v-model="passportSerial" />
+          <input
+            class="myInput passSerial"
+            placeholder="1234"
+            v-model="passportSerial"
+          />
         </div>
         <div class="passport number">
           Номер пасспорта
-          <input class="myInput passNumber" placeholder="567890" v-model="passportNumber" />
+          <input
+            class="myInput passNumber"
+            placeholder="567890"
+            v-model="passportNumber"
+          />
         </div>
         <div class="passport get-date">
           Дата выдачи
-          <input class="myInput dateGet" placeholder="дд.мм.гггг" v-model="dateGet" />
+          <input
+            class="myInput dateGet"
+            placeholder="дд.мм.гггг"
+            v-model="dateGet"
+          />
         </div>
       </div>
 
@@ -75,36 +166,126 @@
         <div class="string four">
           <div class="input-wrapper second-name-latin" style="width: 320px">
             Surname in Latin
-            <input class="myInput second-name-latin" placeholder="Ivanov" v-model="secondNameLatin" />
+            <input
+              class="myInput second-name-latin"
+              placeholder="Ivanov"
+              v-model="secondNameLatin"
+            />
           </div>
           <div class="input-wrapper first-name-latin" style="width: 320px">
             Name in Latin
-            <input class="myInput first-name-latin" placeholder="Ivan" v-model="firstNameLatin" />
+            <input
+              class="myInput first-name-latin"
+              placeholder="Ivan"
+              v-model="firstNameLatin"
+            />
           </div>
         </div>
         <div class="string four2">
           <div class="input-wrapper passport number-latin">
             Passport number
-            <input class="myInput passSerialLatin" placeholder="" v-model="passportNumberLatin" />
+            <input
+              class="myInput passSerialLatin"
+              placeholder=""
+              v-model="passportNumberLatin"
+            />
           </div>
-          <div class="input-wrapper passport country-get">
+          <div class="input-wrapper passport country-get" v-click-outside="hideDropdownCountryGet">
             Country of issue
-            <input class="myInput countryGet" placeholder="" v-model="countryGet" />
+
+            <div
+              class="dropdown-wrapper"
+              @click="isDropdownCountryGetOpen=true"
+            >
+              <input
+                class="myInput dropdown countryGet"
+                placeholder=""
+                v-model="countryGet"
+                @focus="isDropdownCountryGetOpen=true"
+              />
+
+              <div class="triangle"></div>
+            </div>
+
+            <div
+              class="dropdown"
+              v-if="isDropdownCountryGetOpen"
+            >
+              <ul class="dropdown-content">
+                <li
+                  v-for="citizen in citizenshipsData"
+                  :key="citizen.id"
+                  class="li-in-content"
+                  @click="clickHandlerOnCountryGetOpen(citizen.flag)"
+                >
+                  {{citizen.flag}}
+                </li>
+              </ul>
+            </div>
+
           </div>
-          <div class="input-wrapper passport type">
+
+          <div
+            class="input-wrapper passport type"
+            v-click-outside="hideDropdownPassType"
+          >
             Passport type
-            <input class="myInput passType" placeholder="" v-model="passportType" />
+
+            <div
+              class="dropdown-wrapper"
+              @click="isDropdownPassTypeOpen=true"
+            >
+              <input
+                class="myInput dropdown passType"
+                placeholder=""
+                v-model="passportTypeText"
+                @focus="isDropdownPassTypeOpen=true"
+              />
+              <div class="triangle"></div>
+            </div>
+
+            <div
+              class="dropdown"
+              v-if="isDropdownPassTypeOpen"
+            >
+              <ul class="dropdown-content">
+                <li
+                  v-for="passport in passportTypesData"
+                  :key="passport.id"
+                   class="li-in-content"
+                  @click="clickHandlerOnPassportType(passport.type)"
+                >
+                  {{passport.type}}
+                </li>
+              </ul>
+            </div>
+
           </div>
         </div>
       </div>
     </div>
 
-    <div class="personal-change" style="display: flex; flex-direction: column;margin-top: 10px">
+    <div
+      class="personal-change"
+      style="display: flex; flex-direction: column; margin-top: 10px"
+    >
       <h3>Меняли ли фамилию или имя?</h3>
       <div class="line five">
-        <input type="radio" value="false" id="radio-no" class="radio" v-model="isChanged" />
+        <input
+          type="radio"
+          value="false"
+          id="radio-no"
+          class="radio"
+          v-model="isChanged"
+        />
         <label for="radio-no" class="label-radio">Нет</label>
-        <input type="radio" value="true" id="radio-yes" class="radio" v-model="isChanged" />
+        <input
+          type="radio"
+          value="true"
+          id="radio-yes"
+          class="radio"
+          v-model="isChanged"
+        />
         <label for="radio-yes" class="label-radio">Да</label>
       </div>
       <div class="isChanged" v-show="isChanged === 'true'">
@@ -112,12 +293,20 @@
           <!--      ++++++++++++++++++++++++-->
           <div class="input-wrapper second-name" style="width: 320px">
             Предыдущая фамилия
-            <input class="myInput second-name" placeholder="Иванов" v-model="secondNameChanged" />
+            <input
+              class="myInput second-name"
+              placeholder="Иванов"
+              v-model="secondNameChanged"
+            />
           </div>
           <!--      ++++++++++++++++++++++++-->
           <div class="input-wrapper first-name" style="width: 320px">
             Предыдущее имя
-            <input class="myInput first-name" placeholder="Иван" v-model="firstNameChanged" />
+            <input
+              class="myInput first-name"
+              placeholder="Иван"
+              v-model="firstNameChanged"
+            />
           </div>
           <!--      ++++++++++++++++++++++++-->
         </div>
@@ -125,24 +314,38 @@
     </div>
     <div
       v-show="firstName && secondName && patronymicName"
-      style="display: flex; justify-content: space-between;align-items: center;"
+      style="display: flex; justify-content: space-between; align-items: center"
     >
-      <div style="margin-right: 20px;">
-        Я, {{ secondName }} {{ firstName }} {{ patronymicName }} соглашаюсь отправить форму
+      <div style="margin-right: 20px">
+        Я, {{ secondName }} {{ firstName }} {{ patronymicName }} соглашаюсь
+        отправить форму
       </div>
 
-      <input type="submit" style="height: 30px" />
+      <input type="submit" @click="formSubmit" style="height: 30px" />
+
     </div>
   </div>
 </template>
 
 <script>
 import citizenships from "@/assets/data/citizenships.json";
+import passportTypes from "@/assets/data/passport-types.json";
+import ClickOutside from "vue-click-outside";
 
 export default {
+  directives:{
+    ClickOutside
+  },
   data() {
     return {
       citizenshipsData: [],
+      passportTypesData:[],
+      citizenshipsArr:[
+        {country:"Россия"},
+        {country:"Другое"},
+      ],
+      submitDataRus:{},
+      submitDataInt:{},
       firstName: "",
       firstNameChanged: "",
       firstNameLatin: "",
@@ -156,25 +359,111 @@ export default {
       passportSerial: "",
       passportNumber: "",
       passportNumberLatin: "",
-      passportType: "",
+      passportTypeText: "",
       countryGet: "",
       dateGet: "",
       sexText: "",
-      isChanged: "false"
+      isChanged: "false",
+      isDropdownPassTypeOpen: false,
+      isDropdownCountryGetOpen:false,
+      isDropdownCitizenshipOpen:false,
     };
   },
   mounted() {
     this.citizenshipsData = citizenships;
+    this.passportTypesData = passportTypes;
   },
-  methods: {},
-  computed: {}
+  methods: {
+    hideDropdownPassType(){
+      this.isDropdownPassTypeOpen=false;
+    },
+    hideDropdownCountryGet(){
+      this.isDropdownCountryGetOpen = false;
+    },
+    hideDropdownCitizenship(){
+      this.isDropdownCitizenshipOpen = false;
+    },
+    clickHandlerOnPassportType(type){
+      this.passportTypeText = type;
+      this.isDropdownPassTypeOpen = false;
+    },
+    clickHandlerOnCountryGetOpen(country){
+      this.countryGet = country;
+      this.isDropdownCountryGetOpen = false;
+    },
+    clickHandlerOnCitizenship(citizenship){
+      if(citizenship){
+        this.citizenshipText = citizenship.nationality;
+        this.countryGet = citizenship.flag;
+      }
+      else {
+        this.citizenshipText = "";
+        let el = this.$refs.citizenship;
+        el.focus();
+      };
+      this.isDropdownCitizenshipOpen = false;
+    },
+    formSubmit: function() {
+      let json;
+      if (this.citizenshipText === "Россия" || this.citizenshipText === "Russia") {
+        console.log("rus");
+        this.submitDataRus = {
+          firstname: this.firstName,
+          lastname: this.secondName,
+          secondname: this.patronymicName,
+          birthday: this.birthdayText,
+          email: this.emailText,
+          sex: this.sexText,
+          citizenship: this.citizenshipText,
+
+          passport_serial: this.passportSerial,
+          passport_number: this.passportNumber,
+          date_of_issue: this.dateGet,
+        };
+
+        if(this.isChanged === "true"){
+          this.submitDataRus.is_changed_bio = "true";
+          this.submitDataRus.previos_lastname =this.secondNameChanged;
+          this.submitDataRus.previos_firstname =this.firstNameChanged;
+        } else {this.submitDataRus.is_changed_bio = false };
+
+        json = JSON.stringify(this.submitDataRus);
+      } else {
+        console.log("else");
+        this.submitDataInt = {
+          firstname: this.firstName,
+          lastname: this.secondName,
+          secondname: this.patronymicName,
+          birthday: this.birthdayText,
+          email: this.emailText,
+          sex: this.sexText,
+          citizenship: this.citizenshipText,
+
+          surname_latin: this.secondNameLatin,
+          name_latin: this.firstNameLatin,
+          passport_number: this.passportNumberLatin,
+          country_of_issue: this.countryGet,
+          passport_type: this.passportTypeText
+        };
+        if(this.isChanged === "true"){
+          this.submitDataInt.is_changed_bio = "true";
+          this.submitDataInt.previos_lastname =this.secondNameChanged;
+          this.submitDataInt.previos_firstname =this.firstNameChanged;
+        } else {this.submitDataInt.is_changed_bio = false};
+
+        json = JSON.stringify(this.submitDataInt);
+      }
+      console.log(json);
+    }
+  },
+  computed: {},
 };
 </script>
 
 <style scoped>
 .form {
   box-sizing: border-box;
-  background: white;
+  background: #f9f9f9;
   border: 1px solid #aeb7e6;
   border-radius: 6px;
   padding: 20px;
@@ -199,26 +488,55 @@ input {
 
 .myInput {
   width: auto;
-  font-size: 18px;
+  font-size: 15px;
   padding: 6px 0 4px 10px;
   border: 2px solid #cecece;
-  background: #f8faff;
+  background: #fcfcfc;
   border-radius: 6px;
+  margin-right: 0;
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+.dropdown-wrapper{
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  border: 2px solid #cecece;
+  width: auto;
+  border-radius: 6px;
+  justify-content: space-between;
+  margin-bottom: 5px;
+  margin-top: 10px;
+  background: white;
+}
+.string.four{
+  margin-top: 5px;
+}
+.myInput.dropdown{
+  width: 100%;
+  border: 0px;
+}
+.myInput.dropdown:focus{
+  outline: none
 }
 
 .input-wrapper {
   display: flex;
   flex-direction: column;
+  min-width: 200px;
+  /*color: #cecece;*/
 }
 
 .label-radio {
   margin-right: 20px;
 }
-
+h1,
 h2,
 h3 {
   margin-bottom: 8px;
+  margin-top: 2px;
 }
+
 
 .line {
   margin-bottom: 5px;
@@ -227,6 +545,57 @@ h3 {
 
 .string.six {
   display: flex;
+}
+
+.dropdown {
+  position: relative;
+  display: inline-block;
+  margin: 0;
+}
+
+.dropdown-content {
+  position: absolute;
+  list-style: none;
+  background-color: white;
+  min-width: 100%;
+  max-height: 150px;
+  border-radius: 6px;
+  padding: 0 ;
+  z-index: 1;
+  margin:0;
+  overflow: auto;
+
+}
+.li-in-content{
+  box-shadow: 0px 0px 5px 1px rgba(0, 23, 191, 0.1);
+  border: 1px solid #aeb7e6;
+  background: white;
+  border-radius: 6px;
+  padding-left: 5px;
+  min-height: 30px;
+  display: flex;
+  align-items: center;
+  font-size:15px;
+}
+.li-in-content:hover{
+  border-radius: 6px;
+  background-color: #eef0ff;
+  cursor: pointer;
+}
+
+/*.dropdown:hover .dropdown-content {*/
+/*  display: block;*/
+/*}*/
+.triangle {
+  width: 0;
+  height: 0;
+  border-left: 5px solid transparent;
+  border-right: 5px solid transparent;
+  border-top: 10px solid #bebebe;
+  margin-right: 5px;
+}
+.triangle:hover{
+  cursor: pointer;
 }
 
 </style>
